d4c21ad4e69e581f3f5e3c999f510c983c34d765
luscus <luscus.redbeard@gmail.com>
2014-05-05 17:07:21 +0200

    initial commit

---
+++ test/service.skeleton.test.js
@@ -0,0 +1,33 @@
+var options = {test:'ein kleiner Test'},
+    skeleton = require('../lib/service-skeleton')(options);
+
+
+skeleton.logger.setLogLevel(skeleton.logger.LOG);
+
+skeleton.setShutdownHandler(function () {
+  console.log('YOU ARE MINE...');
+});
+
+var test = 0,
+    calls = 0;
+
+skeleton.metrics.addTask('log', function (metrics) {
+  calls++;
+  console.log('################################################################');
+  skeleton.logger.log(metrics);
+});
+
+skeleton.metrics.set('persistent-counter');
+skeleton.metrics.set('counter');
+skeleton.metrics.set('fackeValues', false);
+
+setInterval(function () {
+  test++;
+  skeleton.metrics.incr('counter');
+  skeleton.metrics.incr('persistent-counter');
+  skeleton.metrics.incr('fackeValues', test);
+
+  if (test === 5) test = 0;
+}, 1000);
+
+skeleton.logger.info('"'+skeleton.probe.name+'" service starting...');
